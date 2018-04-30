80a2917116c3c9e166de20944dd2aeabffadee3d
coda1997 <overlooker1997@gmail,com>
2018-04-30 20:06:56 +0800

    initialization done

---
+++ app/src/main/java/com/example/overl/kefu/Fragment2.kt
@@ -0,0 +1,24 @@
+package com.example.overl.kefu
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by overl on 2018/4/30.
+ */
+class Fragment2 : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater?.inflate(R.layout.fragment_main_2,container,false)
+        view?.apply { initView(this) }//if view is null, skip initialization
+        return view?:throw Exception()
+    }
+
+    //initialize fragment when drawing the fragment
+    private fun initView(view: View){
+
+    }
+
+}
