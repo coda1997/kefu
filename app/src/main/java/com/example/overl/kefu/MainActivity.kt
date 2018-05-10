f93e3a78b849d1873ab13baafd13c8b2bc56eda0
coda1997 <overlooker1997@gmail,com>
2018-05-10 13:53:26 +0800

    init fragment 1

--- app/src/main/java/com/example/overl/kefu/MainActivity.kt
+++ app/src/main/java/com/example/overl/kefu/MainActivity.kt
@@ -5,7 +5,9 @@ import android.app.FragmentManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Window
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.example.overl.kefu.community.Fragment3
 import com.example.overl.kefu.main.Fragment1
 import com.example.overl.kefu.mine.Fragment4
@@ -31,24 +33,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun initView() {
 //        Log.d("start Activity","main")
+
+
         initFragment()
 
     }
 
     private fun initFragment() {
-        fragmentManager.beginTransaction().add(R.id.fragment_main,fragment1).show(fragment1).commit()
-        fgs.add(fragment1)
+        fragmentManager.beginTransaction().add(R.id.fragment_main, fragment1).show(fragment1).commit()
+        fgs.add(fragment1);changeButtonAndImage(1)
         find<LinearLayout>(R.id.button_1).onClick {
-            fragmentManager.selectFragment(fragment1,fgs)
+            fragmentManager.selectFragment(fragment1, fgs)
+            changeButtonAndImage(1)
         }
         find<LinearLayout>(R.id.button_2).onClick {
-            fragmentManager.selectFragment(fragment2,fgs)
+            fragmentManager.selectFragment(fragment2, fgs)
         }
         find<LinearLayout>(R.id.button_3).onClick {
-            fragmentManager.selectFragment(fragment3,fgs)
+            fragmentManager.selectFragment(fragment3, fgs)
         }
         find<LinearLayout>(R.id.button_4).onClick {
-            fragmentManager.selectFragment(fragment4,fgs)
+            fragmentManager.selectFragment(fragment4, fgs)
         }
         find<LinearLayout>(R.id.button_scan).onClick {
             startActivity<ScanActivity>()
@@ -56,14 +61,29 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
+    val image1: ImageView by lazy { find<ImageView>(R.id.button_1_im) }
+    val image2: ImageView by lazy { find<ImageView>(R.id.button_2_im) }
+    val image3: ImageView by lazy { find<ImageView>(R.id.button_3_im) }
+    val image4: ImageView by lazy { find<ImageView>(R.id.button_4_im) }
+    val text1: TextView by lazy { find<TextView>(R.id.button_1_tv) }
+    val text2: TextView by lazy { find<TextView>(R.id.button_2_tv) }
+    val text3: TextView by lazy { find<TextView>(R.id.button_3_tv) }
+    val text4: TextView by lazy { find<TextView>(R.id.button_4_tv) }
+    private fun changeButtonAndImage(index: Int) {
+        when (index) {
+            1 -> {
+                image1.setImageResource(R.drawable.nav_ic_sel)
+                text1.setTextColor(resources.getColor(R.color.green))
+            }
+        }
+    }
 }
 
-private fun FragmentManager.selectFragment(fragment:Fragment,fgs:MutableList<Fragment>){
+private fun FragmentManager.selectFragment(fragment: Fragment, fgs: MutableList<Fragment>) {
     val transaction = beginTransaction()
     fgs.forEach { transaction.hide(it) }
     if (!fgs.contains(fragment)) {
-        transaction.add(R.id.fragment_main,fragment)
+        transaction.add(R.id.fragment_main, fragment)
         fgs.add(fragment)
     }
     transaction.show(fragment)
--- app/src/main/java/com/example/overl/kefu/main/Fragment1.kt
+++ app/src/main/java/com/example/overl/kefu/main/Fragment1.kt
@@ -19,6 +19,7 @@ import android.view.animation.Animation
 import android.view.animation.TranslateAnimation
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.example.overl.kefu.MyImageAdapter
 import com.example.overl.kefu.R
 import com.example.overl.kefu.views.ViewPagerForScrollView
@@ -37,7 +38,12 @@ class Fragment1 : Fragment(), ViewPager.OnPageChangeListener {
     private var bmpWidth = 0
     private var one = 0
     private var two = 0
-
+    lateinit var image1: ImageView
+    lateinit var image2: ImageView
+    lateinit var image3: ImageView
+    lateinit var text1: TextView
+    lateinit var text2: TextView
+    lateinit var text3: TextView
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater?.inflate(R.layout.fragment_main, container, false)
         view?.apply { initView(this) }//if view is null, skip initialization
@@ -46,6 +52,12 @@ class Fragment1 : Fragment(), ViewPager.OnPageChangeListener {
 
     //initialize fragment when drawing the fragment
     private fun initView(view: View) {
+        image1 = view.find(R.id.tag_main_1_1_im)
+        image2 = view.find(R.id.tag_main_1_2_im)
+        image3 = view.find(R.id.tag_main_1_3_im)
+        text1 = view.find(R.id.tag_main_1_1_tv)
+        text2 = view.find(R.id.tag_main_1_2_tv)
+        text3 = view.find(R.id.tag_main_1_3_tv)
         initTopImageBanner(view)
         initMainViewPagerAndTagHost(view)
 
@@ -59,18 +71,19 @@ class Fragment1 : Fragment(), ViewPager.OnPageChangeListener {
         val screenW = dsm.widthPixels
         offset = (screenW / 3 - bmpWidth) / 2
         val matrix = Matrix()
-        matrix.postTranslate(offset.toFloat(),0f)
+        matrix.postTranslate(offset.toFloat(), 0f)
         view.find<ImageView>(R.id.img_cursor).imageMatrix = matrix
-        one = offset*2+bmpWidth
-        two = one*2
+        one = offset * 2 + bmpWidth - 30
+        two = one * 2 - 30
 
         val list = mutableListOf<View>()
         //module
-        list.add(layoutInflater.inflate(R.layout.view_main_1,null,false).apply { initMainView1(this) })
-        list.add(layoutInflater.inflate(R.layout.view_main_1,null,false).apply { initMainView1(this) })
-        list.add(layoutInflater.inflate(R.layout.view_main_3,null,false).apply { initMainView3(this) })
+        list.add(layoutInflater.inflate(R.layout.view_main_1, null, false).apply { initMainView1(this) })
+        list.add(layoutInflater.inflate(R.layout.view_main_1, null, false).apply { initMainView1(this) })
+        list.add(layoutInflater.inflate(R.layout.view_main_3, null, false).apply { initMainView3(this) })
         vp.adapter = AdapterMain2(list)
         vp.currentItem = 0
+        tagChange(1,0)
         view.find<LinearLayout>(R.id.tag_main_1_1).onClick {
             vp.currentItem = 0
         }
@@ -87,13 +100,13 @@ class Fragment1 : Fragment(), ViewPager.OnPageChangeListener {
     private fun initMainView3(view: View?) {
         val adapter = VideoAdapter(context)
         val rv = view?.find<EasyRecyclerView>(R.id.recyclerView_main_3)
-        rv?.isNestedScrollingEnabled=false
+        rv?.isNestedScrollingEnabled = false
 
-        val layoutManager = GridLayoutManager(context,2)
+        val layoutManager = GridLayoutManager(context, 2)
         rv?.setLayoutManager(layoutManager)
         val list = mutableListOf<VideoEntity>()
         (0 until 10).forEach {
-            list.add(VideoEntity(it,"kefu video"))
+            list.add(VideoEntity(it, "kefu video"))
         }
         adapter.addAll(list)
         rv?.setAdapterWithProgress(adapter)
@@ -102,21 +115,21 @@ class Fragment1 : Fragment(), ViewPager.OnPageChangeListener {
     private val handle = Handler()
     private fun initMainView1(view: View?) {
         val adapter = NewsAdapter(context)
-        adapter.setMore(R.layout.view_more){
+        adapter.setMore(R.layout.view_more) {
             handle.postDelayed({
-                Log.d("load","more")
-            },2000)
+                Log.d("load", "more")
+            }, 2000)
         }
-        val rv  = view?.find<EasyRecyclerView>(R.id.recyclerView_main_1)
-        rv?.isNestedScrollingEnabled=false//???
+        val rv = view?.find<EasyRecyclerView>(R.id.recyclerView_main_1)
+        rv?.isNestedScrollingEnabled = false//???
         val layoutManager = LinearLayoutManager(context)
         rv?.setLayoutManager(layoutManager)
-        val itemDecoration = DividerDecoration(Color.GRAY, Util.dip2px(context,16f), Util.dip2px(context,72f),0)
+        val itemDecoration = DividerDecoration(Color.GRAY, Util.dip2px(context, 16f), Util.dip2px(context, 72f), 0)
         itemDecoration.setDrawLastItem(false)
         rv?.addItemDecoration(itemDecoration)
         val list = mutableListOf<NewsListEntity>()
-        (0 ..10).forEach {
-            list.add(NewsListEntity(it,"kefu",excerpt = "hello world",publishDate = ""))
+        (0..10).forEach {
+            list.add(NewsListEntity(it, "kefu", excerpt = "hello world", publishDate = ""))
         }
         adapter.addAll(list)
         rv?.setAdapterWithProgress(adapter)
@@ -126,7 +139,7 @@ class Fragment1 : Fragment(), ViewPager.OnPageChangeListener {
     @SuppressLint("InflateParams")
     private fun initTopImageBanner(view: View) {
         val vp = view.find<ViewPager>(R.id.rv_main_top_image)
-        vp.pageMargin = 60
+        vp.pageMargin = 30
         vp.setPageTransformer(true, ScaleInPageTransformer())
         val lists = mutableListOf<View>()
         val layoutInflater = layoutInflater
@@ -153,21 +166,68 @@ class Fragment1 : Fragment(), ViewPager.OnPageChangeListener {
     override fun onPageSelected(position: Int) {
         val animation: Animation = when (position) {
             0 -> {
-                if (currIndex == 1) TranslateAnimation(one.toFloat(), 0f, 0f, 0f)
-                else TranslateAnimation(two.toFloat(), 0f, 0f, 0f)
+                if (currIndex == 1) {
+                    tagChange(1, 0)
+                    TranslateAnimation(one.toFloat(), 0f, 0f, 0f)
+                } else {
+                    tagChange(2, 0)
+                    TranslateAnimation(two.toFloat(), 0f, 0f, 0f)
+                }
             }
             1 -> {
-                if (currIndex == 0) TranslateAnimation(offset.toFloat(), one.toFloat(), 0f, 0f)
-                else TranslateAnimation(two.toFloat(), one.toFloat(), 0f, 0f)
+                if (currIndex == 0) {
+                    tagChange(0, 1)
+                    TranslateAnimation(offset.toFloat(), one.toFloat(), 0f, 0f)
+                } else {
+                    tagChange(2, 1)
+                    TranslateAnimation(two.toFloat(), one.toFloat(), 0f, 0f)
+                }
             }
             else -> {
-                if (currIndex == 0) TranslateAnimation(offset.toFloat(), two.toFloat(), 0f, 0f)
-                else TranslateAnimation(one.toFloat(), two.toFloat(), 0f, 0f)
+                if (currIndex == 0) {
+                    tagChange(0, 2)
+                    TranslateAnimation(offset.toFloat(), two.toFloat(), 0f, 0f)
+                } else {
+                    tagChange(1, 2)
+                    TranslateAnimation(one.toFloat(), two.toFloat(), 0f, 0f)
+                }
             }
         }
         currIndex = position
         animation.fillAfter = true
         animation.duration = 300
         view.find<ImageView>(R.id.img_cursor).startAnimation(animation)
+
+    }
+
+    private fun tagChange(from: Int, to: Int) {
+        when (to) {
+            0 -> {
+                image1.setImageResource(R.drawable.menu_ic_sel)
+                text1.setTextColor(resources.getColor(R.color.green))
+            }
+            1 -> {
+                image2.setImageResource(R.drawable.menu_ic_sel_2)
+                text2.setTextColor(resources.getColor(R.color.green))
+            }
+            else -> {
+                image3.setImageResource(R.drawable.menu_ic_sel_3)
+                text3.setTextColor(resources.getColor(R.color.green))
+            }
+        }
+        when (from) {
+            0 -> {
+                image1.setImageResource(R.drawable.menu_ic_nor_1)
+                text1.setTextColor(resources.getColor(R.color.gray))
+            }
+            1 -> {
+                image2.setImageResource(R.drawable.menu_ic_nor_2)
+                text2.setTextColor(resources.getColor(R.color.gray))
+            }
+            else -> {
+                image3.setImageResource(R.drawable.menu_ic_nor_3)
+                text3.setTextColor(resources.getColor(R.color.gray))
+            }
+        }
     }
 }
